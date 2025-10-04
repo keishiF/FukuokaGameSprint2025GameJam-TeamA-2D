9d47d148fae6f2246e4fb4c6267e5b132d57285b
Nroido <flcn8810@gmail.com>
2025-10-04 14:17:51 +0900

    Test

---
+++ Assets/Script/HP Bar.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPBar : MonoBehaviour
+{
+    public static int HP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public static void HitDamage()
+    {
+
+    }
+}
